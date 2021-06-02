b5323e25212ffa2dce8551e4f2a397243483265e
DmitriyChistyakov <dinacher@mail.ru>
2021-06-02 23:46:11 +0300

    HW3.1 Урок3.1 Адаптирован проект к новой архитектуре. Комментарии пока не удалены.

--- core/src/ru/gb/screen/MenuScreen.java
+++ core/src/ru/gb/screen/MenuScreen.java
@@ -8,11 +8,14 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import ru.gb.base.BaseScreen;
 import ru.gb.math.Rect;
 import ru.gb.sprite.Background;
+import ru.gb.sprite.Logo;
 
 public class MenuScreen extends BaseScreen {
 
     private Texture bg;
+    private Logo logo;
     private Background background;
+    private Texture badlogic;
 //    private Texture img;
 //    private Texture imgFon;
 //    private Vector2 pos;
@@ -25,6 +28,8 @@ public class MenuScreen extends BaseScreen {
         super.show();
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
+        badlogic = new  Texture("badlogic.jpg");
+        logo = new Logo(badlogic);
 //        imgFon = new Texture("fon.png");
 //        img = new Texture("badlogic.jpg");
 //        pos = new Vector2(-0.5f, -0.5f);
@@ -36,15 +41,18 @@ public class MenuScreen extends BaseScreen {
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        logo.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
 //        pos.add(v);
+        logo.update(delta);
         ScreenUtils.clear(0.33f, 0.45f, 0.68f, 1);
         batch.begin();
         background.draw(batch);
+        logo.draw(batch);
 //        batch.draw(imgFon, 0, 0);
 //        batch.draw(img, pos.x, pos.y,1f,1f);
         batch.end();
@@ -61,6 +69,7 @@ public class MenuScreen extends BaseScreen {
     public void dispose() {
         super.dispose();
         bg.dispose();
+        badlogic.dispose();
 //        img.dispose();
     }
 
@@ -77,6 +86,7 @@ public class MenuScreen extends BaseScreen {
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
 //        pos.set(touch);
+        logo.touchDown(touch, pointer,button);
         return false;
     }
 }
