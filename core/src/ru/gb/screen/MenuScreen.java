451593d00d89918999a88d5580cdc715444d5b34
DmitriyChistyakov <dinacher@mail.ru>
2021-06-06 13:14:32 +0300

    Урок 4. Часть 1.

--- core/src/ru/gb/screen/MenuScreen.java
+++ core/src/ru/gb/screen/MenuScreen.java
@@ -1,21 +1,25 @@
 package ru.gb.screen;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import ru.gb.base.BaseScreen;
 import ru.gb.math.Rect;
 import ru.gb.sprite.Background;
-import ru.gb.sprite.Logo;
+import ru.gb.sprite.Star;
 
 public class MenuScreen extends BaseScreen {
 
+    private static final int STAR_COUNT = 256;
+
     private Texture bg;
-    private Logo logo;
+    private TextureAtlas atlas;
+    //    private Logo logo;
     private Background background;
-    private Texture badlogic;
+    private Star[] stars;
+//    private Texture badlogic;
 //    private Texture img;
 //    private Texture imgFon;
 //    private Vector2 pos;
@@ -28,8 +32,13 @@ public class MenuScreen extends BaseScreen {
         super.show();
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
-        badlogic = new  Texture("badlogic.jpg");
-        logo = new Logo(badlogic);
+        atlas = new TextureAtlas("textures/menuAtlas.tpack");
+        stars = new Star[STAR_COUNT];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(atlas);
+        }
+//        badlogic = new  Texture("badlogic.jpg");
+//        logo = new Logo(badlogic);
 //        imgFon = new Texture("fon.png");
 //        img = new Texture("badlogic.jpg");
 //        pos = new Vector2(-0.5f, -0.5f);
@@ -41,21 +50,21 @@ public class MenuScreen extends BaseScreen {
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
-        logo.resize(worldBounds);
+        for (Star star: stars) {
+            star.resize(worldBounds);
+        }
+//        star.resize(worldBounds);
+//        logo.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        update(delta);
+        draw();
 //        pos.add(v);
-        logo.update(delta);
-        ScreenUtils.clear(0.33f, 0.45f, 0.68f, 1);
-        batch.begin();
-        background.draw(batch);
-        logo.draw(batch);
-//        batch.draw(imgFon, 0, 0);
-//        batch.draw(img, pos.x, pos.y,1f,1f);
-        batch.end();
+//        logo.update(delta);
+
 //        if (touch.cpy().sub(pos).len() <= v.len()) {
 //            pos.set(touch);
 //            v.setZero();
@@ -69,7 +78,8 @@ public class MenuScreen extends BaseScreen {
     public void dispose() {
         super.dispose();
         bg.dispose();
-        badlogic.dispose();
+        atlas.dispose();
+//        badlogic.dispose();
 //        img.dispose();
     }
 
@@ -86,7 +96,26 @@ public class MenuScreen extends BaseScreen {
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
 //        pos.set(touch);
-        logo.touchDown(touch, pointer,button);
+//        logo.touchDown(touch, pointer,button);
         return false;
+    }
+
+    private void update(float delta) {
+        for (Star star: stars) {
+            star.update(delta);
+        }
+//        star.update(delta);
+
+    }
+
+    private void draw(){
+        ScreenUtils.clear(0.33f, 0.45f, 0.68f, 1);
+        batch.begin();
+        background.draw(batch);
+        for (Star star: stars) {
+            star.draw(batch);
+        }
+        batch.end();
+
     }
 }
---
+++ core/src/ru/gb/sprite/Star.java
@@ -0,0 +1,61 @@
+package ru.gb.sprite;
+
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.gb.base.Sprite;
+import ru.gb.math.Rect;
+import ru.gb.math.Rnd;
+
+public class Star extends Sprite {
+
+    private final Vector2 v;
+    private Rect wordBounds;
+
+
+    public Star(TextureAtlas atlas) {
+        super(atlas.findRegion("star"));
+        v = new Vector2();
+        float vx = Rnd.nextFloat(-0.0005f, 0.0005f);
+        float vy = Rnd.nextFloat(-0.1f, -0.05f);
+        v.set(vx, vy);
+
+    }
+
+    @Override
+    public void update(float delta) {
+        super.update(delta);
+        pos.mulAdd(v, delta);
+        if (getRight() < wordBounds.getLeft()) {
+            setLeft(wordBounds.getRight());
+        }
+        if (getLeft() > wordBounds.getRight()) {
+            setRight(wordBounds.getLeft());
+        }
+        if (getTop() < wordBounds.getBottom()) {
+            setBottom(wordBounds.getTop());
+        }
+        if (getBottom() > wordBounds.getTop()) {
+            setTop(wordBounds.getBottom());
+        }
+        float height = getHeight();
+        height += 0.0001f;
+        if (height >= 0.012f) {
+            height = 0.008f;
+            setHeightProportion(height);
+        }else {
+            setHeightProportion(height);
+        }
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        this.wordBounds = worldBounds;
+        setHeightProportion(Rnd.nextFloat(0.005f, 0.013f));
+        float x = Rnd.nextFloat(worldBounds.getLeft(), worldBounds.getRight());
+        float y = Rnd.nextFloat(worldBounds.getBottom(), worldBounds.getTop());
+        pos.set(x, y);
+    }
+}
